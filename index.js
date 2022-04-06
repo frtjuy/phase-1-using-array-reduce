fa086ebcefcece13f1f6a6d8be0cae460c4e8a34
Alexander Edwards <alexd.edwards1999@gmail.com>
2022-04-06 03:01:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function(added, toBeAdded) {
+    return added + toBeAdded
+}, 0);
+
+//  function reducer() {
+//     const totalBatteries = batteryBatches.reduce(function(added, toBeAdded) {
+//         return added + toBeAdded
+//     }, 0);
+//     return totalBatteries;
+//  }
+
